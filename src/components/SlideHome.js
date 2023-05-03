05251dde3f62b2e498d005743373472d0ad53657
beyourbabe60 <beyourbabe60@gmail.com>
2023-05-04 05:07:32 +0700

    awaww

--- src/components/SlideHome.js
+++ src/components/SlideHome.js
@@ -9,38 +9,33 @@ import 'swiper/css/navigation'
 const ImagesHomeSlide = [
   {
     id: 0,
-    path: '../assets/images/home/sliderhome/1-2.jpg',
+    path: '../assets/images/home/sliderhome/1-2.WebP',
     text: 'sliderhome1',
   },
   {
     id: 1,
-    path: '../assets/images/home/sliderhome/2-2.jpg',
+    path: '../assets/images/home/sliderhome/2-2.WebP',
     text: 'sliderhome2',
   },
   {
     id: 2,
-    path: '../assets/images/home/sliderhome/3-2.jpg',
+    path: '../assets/images/home/sliderhome/3-2.WebP',
     text: 'sliderhome3',
   },
   {
     id: 3,
-    path: '../assets/images/home/sliderhome/4-2.jpg',
+    path: '../assets/images/home/sliderhome/4-2.WebP',
     text: 'sliderhome4',
   },
   {
     id: 4,
-    path: '../assets/images/home/sliderhome/5-2.jpg',
+    path: '../assets/images/home/sliderhome/5-2.WebP',
     text: 'sliderhome5',
   },
   {
     id: 5,
-    path: '../assets/images/home/sliderhome/6-2.jpg',
+    path: '../assets/images/home/sliderhome/6-2.WebP',
     text: 'sliderhome6',
-  },
-  {
-    id: 6,
-    path: '../assets/images/home/sliderhome/7-2.jpg',
-    text: 'sliderhome7',
   },
 ]
 
@@ -68,7 +63,7 @@ export default function SlideHome() {
               <img
                 src={ImagesHome.path}
                 alt={ImagesHome.text}
-                className="3xl:w-[1584px]"
+                className="w-full h-auto"
               />
             </div>
           </SwiperSlide>
