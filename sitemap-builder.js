7a58d487f48c6bc8088d97dc75a3704a00caf559
beyourbabe60 <beyourbabe60@gmail.com>
2023-05-04 01:27:56 +0700

    Delete sitemap-builder.js

--- sitemap-builder.js
+++
@@ -1,6 +0,0 @@
-require('babel-register');
- 
-const router = require('./router').default;
-const Sitemap = require('react-router-sitemap').default;
- 
-(new Sitemap(router).build('https://theinfinitedigital.com/').save('./sitemap.xml'));
